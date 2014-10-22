8a5e614177709d0b9733aa17bda4e5014e70dacd
TTobi200 <tobias.ohm@outlook.de>
2014-10-22 21:57:39 +0200

    - changed button order - use Notezparser to load/save - set stage position to last saved ones

--- Notez/src/de/util/notez/NotezParsers.java
+++ Notez/src/de/util/notez/NotezParsers.java
@@ -24,10 +24,10 @@ public class NotezParsers
 		mapParser.put("0.1", new NotezParserV01());
 		mapParser.put(NotezParserV02.VERSION, new NotezParserV02());
 		mapParser.put("0", mapParser.get("0.1"));
-		
+
 		LinkedList<String> d = new LinkedList<>(mapParser.keySet());
 		d.sort(null);
-		
+
 		latestParser = mapParser.get(d.getLast());
 	}
 
@@ -40,16 +40,19 @@ public class NotezParsers
 	{
 		return getParserForVersion(getVersionOfFile(file)).parse(file);
 	}
-	
+
 	public static NotezParser getParserForVersion(String version)
 	{
 		NotezParser ret = mapParser.get(version);
-		
+
 		if(ret == null)
 		{
-			return getParserForVersion(version.substring(0, version.lastIndexOf('.')));
+			// XXX $DH Version cannot be parsed correctly (Please check)
+			return new NotezParserV02();
+			// return getParserForVersion(version.substring(0,
+			// version.lastIndexOf('.')));
 		}
-		
+
 		return ret;
 	}
 
@@ -63,34 +66,38 @@ public class NotezParsers
 			{
 				return "0";
 			}
-			
+
 			return line;
 		}
 	}
-	
-	public static void save(NotezController controller, String path) throws IOException
+
+	public static void save(NotezController controller, String path)
+		throws IOException
 	{
 		save(controller, new File(path));
 	}
-	
-	public static void save(NotezController controller, File file) throws IOException
+
+	public static void save(NotezController controller, File file)
+		throws IOException
 	{
 		save(controller, file, null);
 	}
-	
-	public static void save(NotezController controller, String path, String version) throws IOException
+
+	public static void save(NotezController controller, String path,
+					String version) throws IOException
 	{
 		save(controller, new File(path), version);
 	}
-	
-	public static void save(NotezController controller, File file, String version) throws IOException
+
+	public static void save(NotezController controller, File file,
+					String version) throws IOException
 	{
 		NotezParser parser = mapParser.get(version);
 		if(parser == null)
 		{
 			parser = latestParser;
 		}
-		
+
 		parser.save(controller, file);
 	}
 }
