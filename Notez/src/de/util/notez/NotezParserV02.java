314aa7ff322e7815cfbde91898ef6e3fcf7e82e2
tobias.ohm <tobias.ohm@Ohm-1.utintra.com>
2014-10-23 07:20:21 +0200

    - fixed load file (no line breaks)

--- Notez/src/de/util/notez/NotezParserV02.java
+++ Notez/src/de/util/notez/NotezParserV02.java
@@ -16,77 +16,79 @@ import de.gui.NotezController;
 
 public class NotezParserV02 extends NotezParserBase
 {
-	public static final String VERSION = "0.2";
+    public static final String VERSION = "0.2";
 
-	@Override
-	public void save(NotezController controller, File file) throws IOException
-	{
-		try (Writer w = new BufferedWriter(new FileWriter(file)))
-		{
-			w.write(new StringBuilder(STRING_VERSION).append(' ')
-				.append(VERSION)
-				.append(System.lineSeparator())
+    @Override
+    public void save(NotezController controller, File file) throws IOException
+    {
+        try (Writer w = new BufferedWriter(new FileWriter(file)))
+        {
+            w.write(new StringBuilder(STRING_VERSION).append(' ')
+                .append(VERSION)
+                .append(System.lineSeparator())
 
-				.append(controller.getStage().getX())
-				.append(' ')
-				.append(controller.getStage().getY())
-				.append(' ')
-				.append(controller.getStage().getWidth())
-				.append(' ')
-				.append(controller.getStage().getHeight())
-				.append(System.lineSeparator())
+                .append(controller.getStage().getX())
+                .append(' ')
+                .append(controller.getStage().getY())
+                .append(' ')
+                .append(controller.getStage().getWidth())
+                .append(' ')
+                .append(controller.getStage().getHeight())
+                .append(System.lineSeparator())
 
-				.append(controller.getNoteText())
-				.toString());
-		}
-	}
+                .append(controller.getNoteText())
+                .toString());
+        }
+    }
 
-	@Override
-	protected NotezData parseImpl(File file) throws IOException
-	{
-		try (BufferedReader r = new BufferedReader(new FileReader(file)))
-		{
-			String version = r.readLine()
-				.substring(STRING_VERSION.length() + 1);
-			if(!version.trim().equals(VERSION))
-			{
-				throw new IOException(
-					"Version-nr. not supported by this parser\r\nparser-version: "
-									+ VERSION + "\r\nfile-version: " + version);
-			}
-			
-			String[] position = r.readLine().split(" ");
-			double x;
-			double y;
-			double w = Double.NaN;
-			double h = Double.NaN;
-			switch(position.length)
-			{
-				case 4:
-				{
-					w = Double.parseDouble(position[2]);
-					h = Double.parseDouble(position[3]);
-				}
-				case 2:
-				{
-					x = Double.parseDouble(position[0]);
-					y = Double.parseDouble(position[1]);
-					break;
-				}
-				default:
-				{
-					throw new IOException();
-				}
-			}
-			
-			StringBuilder sb = new StringBuilder();
-			String line;
-			while((line = r.readLine()) != null)
-			{
-				sb.append(line);
-			}
-			
-			return new NotezData(file.getName(), sb.toString(), new Point2D(x, y), new Point2D(w, h));
-		}
-	}
+    @Override
+    protected NotezData parseImpl(File file) throws IOException
+    {
+        try (BufferedReader r = new BufferedReader(new FileReader(file)))
+        {
+            String version = r.readLine()
+                .substring(STRING_VERSION.length() + 1);
+            if(!version.trim().equals(VERSION))
+            {
+                throw new IOException(
+                    "Version-nr. not supported by this parser\r\nparser-version: "
+                                    + VERSION + "\r\nfile-version: " + version);
+            }
+
+            String[] position = r.readLine().split(" ");
+            double x;
+            double y;
+            double w = Double.NaN;
+            double h = Double.NaN;
+            switch(position.length)
+            {
+                case 4:
+                {
+                    w = Double.parseDouble(position[2]);
+                    h = Double.parseDouble(position[3]);
+                }
+                case 2:
+                {
+                    x = Double.parseDouble(position[0]);
+                    y = Double.parseDouble(position[1]);
+                    break;
+                }
+                default:
+                {
+                    throw new IOException();
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while((line = r.readLine()) != null)
+            {
+                sb.append(line);
+                sb.append("\n");
+            }
+
+            return new NotezData(file.getName(), sb.toString(), new Point2D(x,
+                y), new Point2D(w, h));
+        }
+    }
 }
