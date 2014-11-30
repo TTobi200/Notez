e02159f43a4bd688a643ffe1604e32775ee46afe
TTobi200 <tobias.ohm@outlook.de>
2014-11-30 14:02:58 +0100

    - added notez tray

---
+++ Notez/src/de/gui/NotezTray.java
@@ -0,0 +1,77 @@
+package de.gui;
+
+import java.awt.Image;
+import java.awt.SystemTray;
+import java.awt.Toolkit;
+import java.awt.TrayIcon;
+import java.awt.TrayIcon.MessageType;
+import java.io.IOException;
+
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+
+public class NotezTray
+{
+	private static final String TRAY_ICON =
+					"include/icons/tray.png";
+
+	private static final String TRAY_TOOLTIP =
+					"Service for receiving Notez!";
+
+	private TrayIcon trayIcon;
+	private Stage stage;
+
+	public NotezTray()
+	{
+		init();
+	}
+
+	private void init()
+	{
+		try
+		{
+			Toolkit.getDefaultToolkit();
+
+			if(!SystemTray.isSupported())
+			{
+				return;
+			}
+
+			SystemTray tray = SystemTray.getSystemTray();
+
+			Image image = ImageIO.read(getClass().getClassLoader()
+				.getResource(TRAY_ICON));
+			trayIcon = new TrayIcon(image);
+
+			trayIcon.addActionListener(event -> Platform.runLater(
+				this::showNotez));
+
+			trayIcon.setToolTip(TRAY_TOOLTIP);
+
+			tray.add(trayIcon);
+		}
+		catch(java.awt.AWTException | IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private void showNotez()
+	{
+		if(stage != null && !stage.isShowing())
+		{
+			stage.show();
+		}
+	}
+
+	public void showMsgNewNotez(Stage notezStage, String
+					caption, String msg)
+	{
+		trayIcon.displayMessage(
+			caption, msg, MessageType.INFO);
+
+		this.stage = notezStage;
+	}
+}
--- Notez/src/de/util/NotezRemoteSync.java
+++ Notez/src/de/util/NotezRemoteSync.java
@@ -17,156 +17,157 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.stage.Stage;
 
 import javax.swing.Timer;
 
 import de.gui.NotezFrame;
+import de.gui.NotezTray;
 
 public class NotezRemoteSync extends Timer
 {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    public static final String THREAD_NAME = NotezRemoteSync.class.getName();
+	public static final String THREAD_NAME = NotezRemoteSync.class.getName();
 
-    final static ObservableList<NotezRemoteUser> availableRemoteUser =
-                    FXCollections.observableArrayList();
+	final static ObservableList<NotezRemoteUser> availableRemoteUser =
+					FXCollections.observableArrayList();
 
-    public static final int SERVER_PORT = 55555;
+	public static final int SERVER_PORT = 55555;
 
-    public static ObservableList<File> notezFiles =
-                    FXCollections.observableArrayList();
+	public static ObservableList<File> notezFiles =
+					FXCollections.observableArrayList();
 
-    private ServerSocket receive;
+	private ServerSocket receive;
 
-    public NotezRemoteSync(File localRemoteNotezFold)
-    {
-        super(1000, e ->
-        {
-            Platform.runLater(() ->
-            {
-                try
-                {
-                    for(File f : localRemoteNotezFold.listFiles())
-                    {
-                        if(NotezFileUtil.isNotez(f) &&
-                           !notezFiles.contains(f))
-                        {
-                            NotezFrame.loadAllNotez(localRemoteNotezFold);
-                            notezFiles.add(f);
-                        }
-                    }
-                }
-                catch(IOException e1)
-                {
-                    e1.printStackTrace();
-                }
-            });
-        });
-
-        try
-        {
-            receive = new ServerSocket(SERVER_PORT);
-        }
-        catch(IOException e2)
-        {
-            e2.printStackTrace();
-        }
-
-        // FORTEST add server for receiving notez
-        Thread t = new Thread(() ->
+	public NotezRemoteSync(File localRemoteNotezFold)
+	{
+		super(1000, e ->
 		{
-		    try
-		    {
-		    	Socket socket;
-		    	while((socket = receive.accept()) != null)
-		    	{
-//		            BufferedReader bufferedReader =
-//		                            new BufferedReader(
-//		                                new InputStreamReader(
-//		                                    socket.getInputStream()));
-//		            char[] buffer = new char[200];
-//		            int anzahlZeichen = bufferedReader.read(buffer, 0, 200);
-		    		
-		    		ObjectInputStream in = new ObjectInputStream(
-		    			socket.getInputStream());
-		    		
-		    		// Read data
-//		    		NotezData data = (NotezData)in.readObject();
-
-		    	}
-		    }
-		    catch(Exception e1)
-		    {
-		        e1.printStackTrace();
-		    }
+			Platform.runLater(() ->
+			{
+				try
+				{
+					for(File f : localRemoteNotezFold.listFiles())
+					{
+						if(NotezFileUtil.isNotez(f) &&
+							!notezFiles.contains(f))
+						{
+							NotezFrame.loadAllNotez(localRemoteNotezFold);
+							notezFiles.add(f);
+						}
+					}
+				}
+				catch(IOException e1)
+				{
+					e1.printStackTrace();
+				}
+			});
 		});
-        t.setDaemon(true);
-        t.start();
-    }
 
-    public static synchronized void addUser(NotezRemoteUser user)
-    {
-        availableRemoteUser.add(user);
-    }
+		try
+		{
+			receive = new ServerSocket(SERVER_PORT);
+		}
+		catch(IOException e2)
+		{
+			e2.printStackTrace();
+		}
 
-    public static synchronized void removeUser(NotezRemoteUser user)
-    {
-        availableRemoteUser.remove(user);
-    }
+		// FORTEST add server for receiving notez
+		Thread t = new Thread(() ->
+		{
+			try
+			{
+				NotezTray tray = new NotezTray();
+				Socket socket;
+				while((socket = receive.accept()) != null)
+				{
+					ObjectInputStream in = new ObjectInputStream(
+						socket.getInputStream());
 
-    public static NotezRemoteUser getUser(String username)
-    {
-        for(NotezRemoteUser u : availableRemoteUser)
-        {
-            if(u.getUsername().equals(username))
-            {
-                return u;
-            }
-        }
+					// NotezData data = (NotezData)in.readObject();
 
-        return null;
-    }
+			Platform.runLater(() ->
+			{
+				tray.showMsgNewNotez(new Stage(),
+					"Received new Notez.",
+					"Click here to open");
+			});
+		}
+	}
+	catch(Exception e1)
+	{
+		e1.printStackTrace();
+	}
+}		);
+		t.setDaemon(true);
+		t.start();
+	}
 
-    public static class NotezRemoteUser
-    {
-        private final SimpleStringProperty username;
-        private final SimpleObjectProperty<Object> share;
+	public static synchronized void addUser(NotezRemoteUser user)
+	{
+		availableRemoteUser.add(user);
+	}
 
-        public NotezRemoteUser(String userName, String share)
-        {
-            this.username = new SimpleStringProperty(userName);
-            this.share = new SimpleObjectProperty<Object>(share);
-        }
+	public static synchronized void removeUser(NotezRemoteUser user)
+	{
+		availableRemoteUser.remove(user);
+	}
 
-        public Object getShare()
-        {
-            return share.get();
-        }
+	public static NotezRemoteUser getUser(String username)
+	{
+		for(NotezRemoteUser u : availableRemoteUser)
+		{
+			if(u.getUsername().equals(username))
+			{
+				return u;
+			}
+		}
 
-        public void setShare(String share)
-        {
-            this.share.set(share);
-        }
+		return null;
+	}
 
-        public String getUsername()
-        {
-            return username.get();
-        }
+	public static class NotezRemoteUser
+	{
+		private final SimpleStringProperty username;
+		private final SimpleObjectProperty<Object> share;
 
-        public void setUsername(String username)
-        {
-            this.username.set(username);
-        }
+		public NotezRemoteUser(String userName, String share)
+		{
+			this.username = new SimpleStringProperty(userName);
+			this.share = new SimpleObjectProperty<Object>(share);
+		}
 
-        @Override
-        public String toString()
-        {
-            return getUsername();
-        }
-    }
+		public Object getShare()
+		{
+			return share.get();
+		}
 
-    public static ObservableList<NotezRemoteUser> getAllUsers()
-    {
-        return availableRemoteUser;
-    }
+		public void setShare(String share)
+		{
+			this.share.set(share);
+		}
+
+		public String getUsername()
+		{
+			return username.get();
+		}
+
+		public void setUsername(String username)
+		{
+			this.username.set(username);
+		}
+
+		@Override
+		public String toString()
+		{
+			return getUsername();
+		}
+	}
+
+	public static ObservableList<NotezRemoteUser> getAllUsers()
+	{
+		return availableRemoteUser;
+	}
 }
