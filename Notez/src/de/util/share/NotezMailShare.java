7769aefdfc8021ae8dc92cda9d61a0219a98a99c
tobias.ohm <tobias.ohm@Ohm-1.utintra.com>
2014-12-04 11:13:46 +0100

    Mail Share - added sample for mail-share (not running) - added javax.mail library

--- Notez/src/de/util/share/NotezMailShare.java
+++ Notez/src/de/util/share/NotezMailShare.java
@@ -13,17 +13,53 @@ import de.gui.controller.NotezController;
 
 public class NotezMailShare extends NotezShareBase
 {
-	protected String mailAdress;
+    protected String mailAdress;
 
-	public NotezMailShare(String mailAdress)
-	{
-		this.mailAdress = mailAdress;
-	}
+    public NotezMailShare(String mailAdress)
+    {
+        this.mailAdress = mailAdress;
+    }
 
-	@Override
-	public NotezShareResult shareNotez(NotezController ctrl, File notez)
-		throws IOException
-	{
-		return null;
-	}
+    @Override
+    public NotezShareResult shareNotez(NotezController ctrl, File notez)
+        throws IOException
+    {
+        // TODO add e-mail share
+        // Properties props = new Properties();
+        // Session session = Session.getDefaultInstance(props, null);
+        //
+        // try
+        // {
+        // Message msg = new MimeMessage(session);
+        // // TODO add sender e-mail here
+        // msg.setFrom(new InternetAddress(mailAdress,
+        // genName(mailAdress)));
+        //
+        // msg.addRecipient(Message.RecipientType.TO,
+        // new InternetAddress(mailAdress,
+        // genName(mailAdress)));
+        //
+        // // TODO add username of sender here
+        // msg.setSubject("New Notez from Username");
+        // msg.setText("TEST");
+        // Transport.send(msg);
+        //
+        // return NotezShareResult.SHARED;
+        // }
+        // catch(AddressException e)
+        // {
+        // e.printStackTrace();
+        // }
+        // catch(MessagingException e)
+        // {
+        // e.printStackTrace();
+        // }
+
+        return NotezShareResult.NOT_SUPPORTED;
+    }
+
+    // private String genName(String mail)
+    // {
+    // return mail.substring(0, mail.indexOf("@"));
+    // }
 }
