f89f6746690a14dffad3be45ce16838d2a001e25
tobias.ohm <tobias.ohm@Ohm-1.utintra.com>
2014-12-11 09:54:29 +0100

    Logging - added logging for running in jar

--- Notez/src/de/Startup.java
+++ Notez/src/de/Startup.java
@@ -5,7 +5,15 @@
  */
 package de;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.prefs.Preferences;
 
 import javafx.application.Application;
@@ -21,6 +29,12 @@ public class Startup
     public static final String PREFERENCES_NODE_PREFIX = "Notez";
     public static final String PREFERENCE_NODE_RUNNING = PREFERENCES_NODE_PREFIX
                                                          + ".Running";
+
+    public static final boolean REORG_LOGS = true;
+    public static final int DAYS_TO_SAVE_LOGS = 7;
+    public static final String ERR_FILE_EXT = ".error";
+    public static final String DEBUG_FILE_EXT = ".debug";
+    public static final String LOGGING_FOLDER = "./logging";
 
     public static void main(String[] args) throws IOException,
         InterruptedException
@@ -40,9 +54,56 @@ public class Startup
 
         }
         // FORTEST when running executable jar
-        // System.setErr(new PrintStream("./err.txt"));
+        // logSystemOut(LOGGING_FOLDER, DAYS_TO_SAVE_LOGS, REORG_LOGS);
+
         Application.launch(NotezLoadSplash.class, args);
         // Application.launch(NotezFrame.class, args);
+    }
+
+    public static void logSystemOut(String folder, int daysToLog, boolean reorg)
+        throws IOException
+    {
+        File logFolder = new File(folder);
+        logFolder.mkdir();
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd_HHmmss");
+        Date date = new Date();
+
+        System.setErr(new PrintStream(logFolder.getAbsolutePath()
+                                      + File.separator
+                                      + dateFormat.format(date) + ERR_FILE_EXT));
+        System.setOut(new PrintStream(logFolder.getAbsolutePath()
+                                      + File.separator
+                                      + dateFormat.format(date)
+                                      + DEBUG_FILE_EXT));
+
+        File[] logfiles = logFolder.listFiles();
+        if(reorg)
+        {
+            if(logfiles.length > (daysToLog * 2))
+            {
+                FileTime oldest = FileTime.fromMillis(System.currentTimeMillis());
+                String oldestFileName = "";
+                for(File log : logfiles)
+                {
+                    BasicFileAttributes attr = Files.readAttributes(
+                        log.toPath(), BasicFileAttributes.class);
+
+                    if(oldest.compareTo(attr.creationTime()) > 0)
+                    {
+                        oldest = attr.creationTime();
+                        oldestFileName = log.getName()
+                            .replace(ERR_FILE_EXT, "")
+                            .replace(DEBUG_FILE_EXT, "");
+                    }
+                }
+
+                new File(logFolder.getAbsolutePath() + File.separator
+                         + oldestFileName + ERR_FILE_EXT).delete();
+                new File(logFolder.getAbsolutePath() + File.separator
+                         + oldestFileName + DEBUG_FILE_EXT).delete();
+            }
+        }
     }
 
     private static boolean checkNotezAlreadyRunning()
