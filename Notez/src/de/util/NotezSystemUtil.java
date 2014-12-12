17c647343dbf94484d191b2f4cfca0a361b84a4f
Dauerdaddlah <hertel-dennis@web.de>
2014-12-12 12:32:39 +0100

    NotezSystemUtil - added missing files

---
+++ Notez/src/de/util/NotezSystemUtil.java
@@ -0,0 +1,16 @@
+package de.util;
+
+public class NotezSystemUtil
+{
+	public static final int FATAL = 1;
+
+	public static void exit()
+	{
+		exit();
+	}
+	
+	public static void exit(int status)
+	{
+		System.exit(status);
+	}
+}
